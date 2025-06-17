4ceb28fb407d4f103316b7ce594dc8663c07f310
Rathnasri <12janr34@gmail.com>
2025-06-17 13:37:50 +0530

    commit for quiz app

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/quizApp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('MongoDB connected ');
+});
+
+module.exports = db;
