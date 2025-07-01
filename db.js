fdeb5ec76897b6fe9e06b0e9dd9ca1f65eed148a
Rathnasri12 <12ratnasri17@gmail.com>
2025-07-01 15:19:38 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/quizApp', {
+mongoose.connect('mongodb+srv://ratna:ratna$ri%40123@quiz-app.tmhumpw.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
